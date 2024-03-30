89dac6a2d1dfc457cf195c1dab7ff72e56758f3d
waready <elbarto119@hotmail.com>
2024-03-30 16:09:00 -0500

    inquirer

--- 2-task/app.js
+++ 2-task/app.js
@@ -1,15 +1,16 @@
 require('colors');
 
 const {mostrarMenu, pausa} = require('./helpers/mensajes');
+const { inquirerMenu } = require('./helpers/inquirer');
 
 console.clear();
 
 const main = async () =>{
-    console.log("hola mundo!")
+    //console.log("hola mundo!")
    
     let opt = ''
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         if( opt !== '0')
         await pausa();
     } while (opt !== '0' );
--- 2-task/helpers/inquirer.js
+++ 2-task/helpers/inquirer.js
@@ -2,10 +2,11 @@ const inquirer = require('inquirer');
 require("colors");
 
 const menuOpts = [
-    {type:"list",
+    {
+    type:"list",
     name:"opcion",
     message:"¿Qué decea hacer?",
-    choices:['opt']
+    choices:['opt1','opt2','opt3']
     }
 ]
 
@@ -15,4 +16,10 @@ const inquirerMenu  = async () => {
     console.log("=============================".green);
     console.log("    Selecione una Opcion     ".green);
     console.log("=============================\n".green);
+    const opt = await inquirer.prompt(menuOpts);
+    return opt;
+}
+
+module.exports = {
+    inquirerMenu
 }
